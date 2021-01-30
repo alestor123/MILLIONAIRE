07899c0164fd54126cb312e9a302c2580b06d829
alestor123 <alestoraldous@gmail.com>
2021-01-30 14:06:43 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+var quiz = require('./App'),
+select = require('cli-select'),
+chalk = require('chalk');
+
+(async () => {
+var data = await quiz();
+console.log(chalk.cyanBright.bold(data.question))
+var ans = await select({values: data.options,});
+if(data.correct==ans.value) console.log(chalk.yellowBright.bold('🎉🎉 Correct Good Job 🎉🎉'))
+else console.log(chalk.redBright.bold(`The Answer ${ans.value} is Wrong`) + chalk.greenBright('\nThe Correct Answer Is : ')+chalk.greenBright.bold(data.correct) + chalk.green('\nTry Next Time'))
+})();
